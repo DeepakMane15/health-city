b61bc1c023da8c77e30529c22880d9d85f29a88c
DeepakFromZeus <manedeep2001@gmail.com>
2024-09-18 00:14:22 +0530

    remove environment.ts file from gitignore

---
+++ src/environments/environment.ts
@@ -0,0 +1,24 @@
+// environment.ts
+export const environment = {
+  production: false,
+  apiUrl: 'https://app.profmedservices.com/api',
+  localBEUrl: 'http://localhost:3100/payment',
+  googleMapsApiKey: 'AIzaSyBAztsIXonxMQ3DP70bFYgqClDw1QvCIp4',
+  stripeAPIUrl: 'https://api.inventorysolutions.in/api/payment',
+  baseUrl: 'https://app.profmedservices.com/api',
+  publishableKey:
+    'pk_test_51OctuNBehmEnF60Gw0AqVz8Xhg159Q5upmYipFOySbbPBKQxOW6QbIokmbt8Sr6mXQuErAmufwBYx5XOyp6SDdtJ0099rPdZNI',
+    publishableLiveKey: "pk_live_51OctuNBehmEnF60Gz1OKnCwMsoVitwKebc22qZzblRr9kQox4odsRr7oggmldYHxpnupjBUhrG5HP8C844RHuLrR00iQaUk4Ah",
+  // Other environment variables
+
+  firebase : {
+    apiKey: "AIzaSyDtVQAy3rQZ9Cz8bJBRfFcc6O7UfTKNQSU",
+    authDomain: "profmedservices-com.firebaseapp.com",
+    databaseURL: "https://profmedservices-com-default-rtdb.firebaseio.com",
+    projectId: "profmedservices-com",
+    storageBucket: "profmedservices-com.appspot.com",
+    messagingSenderId: "628534266073",
+    appId: "1:628534266073:web:f51810b8e0d804ec4f16c8",
+    measurementId: "G-YK2DTDYD4P"
+  }
+};
